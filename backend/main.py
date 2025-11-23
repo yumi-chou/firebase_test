52a88e2f4da0fd3bb87a810f1538d01dfdd8a1a7
yumi <yumisunky@gmail.com>
2025-11-23 16:28:14 +0800

    add frontend vercel

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://firebase-test-jflf.vercel.app/",
+        "https://firebase-test-jflf.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
